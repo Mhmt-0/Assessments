476648dca1a1c5251fc6a73a79703f73fd611f18
Mehmet <132574963+Mhmt-0@users.noreply.github.com>
2025-03-29 17:24:57 +0300

    Update 3 Flappy Bird Game.py
    Added jumping mechanics to the bird.

--- Flappy Bird Game.py
+++ Flappy Bird Game.py
@@ -14,12 +14,16 @@ class Bird:
         self.x = x
         self.y = y
         self.radius = 15
-        self.velocity = 0 
+        self.velocity = 0
         self.gravity = 0.5
+        self.jump_strength = -8
 
     def update(self):
         self.velocity += self.gravity
         self.y += self.velocity
+
+    def jump(self):
+        self.velocity = self.jump_strength
 
     def draw(self, screen):
         pygame.draw.circle(screen, RED, (self.x, int(self.y)), self.radius)
@@ -35,12 +39,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird.jump()
     
     bird.update()
     bird.draw(screen)
     
     pygame.display.update()
-    clock.tick(30) 
+    clock.tick(30)
     
 pygame.quit()
 
